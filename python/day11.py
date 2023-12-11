c14bd8658d0c280bcdee99c9cde22047296eb835
JamesZhang2 <jameszhang2019@outlook.com>
2023-12-11 00:21:43 -0500

    Add Day 11 Part 2 Solution

--- python/day11.py
+++ python/day11.py
@@ -2,17 +2,14 @@ with open("input11.txt") as file:
     lines = [line.strip() for line in file.readlines()]
 
 grid = []
-for line in lines:
+empty_rows = set()
+empty_cols = set()
+expansion = 1000000
+for i in range(len(lines)):
+    line = lines[i]
     grid.append([ch for ch in line])
     if '#' not in line:
-        grid.append([ch for ch in line])
-
-# print(len(grid))
-
-new_grid = []
-
-for r in range(len(grid)):
-    new_grid.append([])
+        empty_rows.add(i)
 
 for c in range(len(grid[0])):
     found = False
@@ -21,25 +18,37 @@ for c in range(len(grid[0])):
             found = True
             break
     if not found:
-        for r in range(len(grid)):
-            new_grid[r].append(".")
-    for r in range(len(grid)):
-        new_grid[r].append(grid[r][c])
+        empty_cols.add(c)
 
 pos = []
-for r in range(len(new_grid)):
-    for c in range(len(new_grid[0])):
-        if new_grid[r][c] == '#':
+for r in range(len(grid)):
+    for c in range(len(grid[0])):
+        if grid[r][c] == '#':
             pos.append((r, c))
 
 
-def dist(pt1, pt2):
-    return abs(pt1[0] - pt2[0]) + abs(pt1[1] - pt2[1])
+def dist(pt1, pt2, expansion):
+    d = 0
+    r_min = min(pt1[0], pt2[0])
+    r_max = max(pt1[0], pt2[0])
+    for r in range(r_min, r_max):
+        if r in empty_rows:
+            d += expansion
+        else:
+            d += 1
+    c_min = min(pt1[1], pt2[1])
+    c_max = max(pt1[1], pt2[1])
+    for c in range(c_min, c_max):
+        if c in empty_cols:
+            d += expansion
+        else:
+            d += 1
+    return d
 
 
 sum = 0
 for pt1 in pos:
     for pt2 in pos:
-        sum += dist(pt1, pt2)
+        sum += dist(pt1, pt2, expansion=expansion)
 
 print(sum / 2)
