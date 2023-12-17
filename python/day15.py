373b39612a6de39c7680727ac6713a8a62478fd7
JamesZhang2 <jameszhang2019@outlook.com>
2023-12-17 12:57:56 +0800

    Add Day 15 Part 2 Solution

--- python/day15.py
+++ python/day15.py
@@ -1,4 +1,4 @@
-with open("test15.txt") as file:
+with open("input15.txt") as file:
     lines = [line.strip() for line in file.readlines()]
 
 def get_hash(s):
@@ -11,7 +11,49 @@ def get_hash(s):
 
 words = lines[0].split(",")
 
+# Part 1
+
 ans = 0
 for word in words:
     ans += get_hash(word)
 print(ans)
+
+# Part 2
+
+boxes = []
+for i in range(256):
+    boxes.append([])  # list of [label, focal length] pairs
+    # Not using tuples since python tuples are immutable
+
+for word in words:
+    if '-' in word:
+        label = word[:-1]
+        key = get_hash(label)
+        for pair in boxes[key]:
+            if pair[0] == label:
+                boxes[key].remove(pair)
+                break
+    else:
+        assert '=' in word
+        label = word[:-2]
+        focal_len = int(word[-1])
+        key = get_hash(label)
+
+        found = False
+        for pair in boxes[key]:
+            if pair[0] == label:
+                pair[1] = focal_len
+                found = True
+                break
+        if not found:
+            boxes[key].append([label, focal_len])
+
+# print(boxes)
+
+power = 0
+for i in range(256):
+    for j in range(len(boxes[i])):
+        power += (i + 1) * (j + 1) * boxes[i][j][1]
+
+print(power)
+
